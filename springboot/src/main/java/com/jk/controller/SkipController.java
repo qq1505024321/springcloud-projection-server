19a67cdb46f036ed498d394803a4414ec3f1ab80
zyp <964577441@qq.com>
2019-12-25 10:15:56 +0800

    实训二最后一次更改项目

--- springboot/src/main/java/com/jk/controller/SkipController.java
+++ springboot/src/main/java/com/jk/controller/SkipController.java
@@ -14,9 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class SkipController {
 
-
     @RequestMapping("toShowUser")
     public String toShowUser(){
         return "showUser";
     }
+
 }
